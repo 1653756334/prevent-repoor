8fec744e7ce5b46534d23db1302dc86f4e06138a
世兴 欧 <1653756334@qq.com>
2022-04-11 18:41:45 +0800

    change settings

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src")
     },
   },
+  server: {
+    host: '0.0.0.0',
+  }
 })
